9a644a426a4410c65f11bed830535e239c506fdd
pj <pjpitzer04@gmail.com>
2025-10-29 12:28:03 -0400

    Updated random_tts and add run.py

--- random_tts.py
+++ random_tts.py
@@ -1,18 +1,26 @@
 from gtts import gTTS
 import os
 import random
+import time
 
-random_speech = random.randint(0,2)
-speech = ["Boo","Hello", "Test"]
-play_speech= speech[random_speech]
+times_played=0
+go=1
+speech = ["Boo","Do you like spiders","I see you","Identity confirmed, scanning database","User has been added to the database"]
 
 language = 'en'  # English
 
-# Create a gTTS object
-tts_obj = gTTS(text=play_speech, lang=language, slow=False)
+while go==1:
+    random_speech = random.randint(0,4)
+    play_speech= speech[random_speech]
+    times_played=times_played+1
 
-# Save the audio to an MP3 file
-tts_obj.save("output.mp3")
+    # Create a gTTS object
+    tts_obj = gTTS(text=play_speech, lang=language, slow=True, tld='ie')
 
-# Play the audio (requires a local media player)
-os.system("paplay output.mp3")
+    # Save the audio to an MP3 file
+    tts_obj.save("output.mp3")
+
+    # Play the audio (requires a local media player)
+    os.system("paplay output.mp3")
+    print(f"Sound played {times_played}")
+    time.sleep(5)
